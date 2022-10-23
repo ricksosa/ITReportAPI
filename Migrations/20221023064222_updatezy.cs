9061962fa0f9f89c2c91cda371916566cd55b9c2
Ricardo Sosa <ric.sosa98@gmail.com>
2022-10-23 00:42:57 -0600

    update

---
+++ Migrations/20221023064222_updatezy.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITReportAPI.Migrations
+{
+    public partial class updatezy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "EstadosReporte",
+                columns: new[] { "Id", "Nombre" },
+                values: new object[] { 4, "Nuevo" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "EstadosReporte",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
